1a894a941d6e775c2eaac8b9e7e7aa6af1e8d032
Qixun Qu <quqixun@gmail.com>
2018-03-24 22:43:37 +0100

    Test script for SVM

---
+++ SVM/Test.py
@@ -0,0 +1,20 @@
+# Conventional Machine Learning Algorithms
+# Test script for Class of "SVM".
+# Author: Qixun Qu
+# Create on: 2018/03/24
+# Modify on: 2018/03/24
+
+#     ,,,         ,,,
+#   ;"   ';     ;'   ",
+#   ;  @.ss$$$$$$s.@  ;
+#   `s$$$$$$$$$$$$$$$'
+#   $$$$$$$$$$$$$$$$$$
+#  $$$$P""Y$$$Y""W$$$$$
+#  $$$$  p"$$$"q  $$$$$
+#  $$$$  .$$$$$.  $$$$'
+#   $$$DaU$$O$$DaU$$$'
+#    '$$$$'.^.'$$$$'
+#       '&$$$$$&'
+
+
+from SVM import SVM
