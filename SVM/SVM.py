5214ace1faa94575159156a0bf774b1689ee7aa8
Qixun Qu <quqixun@gmail.com>
2018-04-01 20:42:15 +0200

    Update algorithm but there are still mistakes in take_step

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -24,7 +24,7 @@ class SVC(object):
 
     def __init__(self, C=1.0,
                  kernel="rbf", degree=3,
-                 gamma="auto", coef0=1.0,
+                 sigma="auto", coef0=1.0,
                  tol=1e-2, epsilon=1e-2,
                  random_state=None,
                  verbose=True,
@@ -38,7 +38,7 @@ class SVC(object):
         self.C = C
         self.kernel = kernel
         self.degree = degree
-        self.gamma = gamma
+        self.sigma = sigma
         self.coef0 = coef0
         self.tol = tol
         self.epsilon = epsilon
@@ -66,11 +66,11 @@ class SVC(object):
         self.X = X_train
         self.y = y_train
 
-        self.b = 0.0
+        self.b = 1.0
         self.N, self.F = X_train.shape
 
-        if self.gamma == "auto":
-            self.gamma = 1.0 / self.F
+        if self.sigma == "auto":
+            self.sigma = 1.0 / self.F
 
         self.alphas = np.zeros(self.N)
         self.E = self._E()
@@ -90,17 +90,17 @@ class SVC(object):
         ao = alphas ** 2
         yo = self.y ** 2
         ko = self._K(self.X, self.X)
-        obj = 0.5 * np.sum(ao * yo * ko) - np.sum(alphas)
+        obj = 0.5 * np.sum(ao * ko * yo) - np.sum(alphas)
         return obj
 
-    def _E(self, index="all"):
+    def _E(self, index=None):
         '''_E
 
             Compute error.
 
         '''
 
-        if index == "all":
+        if index is None:
             X, y = self.X, self.y
         else:
             X, y = self.X[index], self.y[index]
@@ -108,7 +108,7 @@ class SVC(object):
         return self._G(X=X) - y
         # return loss
 
-    def _G(self, index="all", X=None):
+    def _G(self, index=None, X=None):
         '''_G
 
             Decision function
@@ -116,7 +116,7 @@ class SVC(object):
         '''
 
         if index is not None:
-            if index == "all":
+            if index is None:
                 X = self.X
             else:
                 X = self.X[index]
@@ -141,9 +141,9 @@ class SVC(object):
         elif self.kernel == "poly":
             return (np.dot(x1, x2.T) + self.coef0) ** self.degree
         elif self.kernel == "sigmoid":
-            return np.tanh(self.gamma * np.dot(x1, x2.T) + self.coef0)
+            return np.tanh(self.sigma * np.dot(x1, x2.T) + self.coef0)
         elif self.kernel == "rbf":
-            deno = 2 * (self.gamma ** 2)
+            deno = 2 * (self.sigma ** 2)
             x1_ndim, x2_ndim = np.ndim(x1), np.ndim(x2)
             if x1_ndim == 1 and x2_ndim == 1:
                 return np.exp(-np.linalg.norm(x1 - x2) / deno)
@@ -205,18 +205,18 @@ class SVC(object):
             else:
                 a2_new = a2_old
 
-        if np.abs(a2_new - a2_old) < \
-           self.epsilon * (a2_new + a2_old + self.epsilon):
+        if (np.abs(a2_new - a2_old) <
+           self.epsilon * (a2_new + a2_old + self.epsilon)):
             return 0
 
         a1_new = a1_old + s * (a2_old - a2_new)
 
-        b1_new = self.b + E1 + \
-            y1 * k11 * (a1_new - a1_old) + \
-            y2 * k12 * (a2_new - a2_old)
-        b2_new = self.b + E2 + \
-            y1 * k12 * (a1_new - a1_old) + \
-            y2 * k22 * (a2_new - a2_old)
+        b1_new = (self.b + E1 +
+                  y1 * k11 * (a1_new - a1_old) +
+                  y2 * k12 * (a2_new - a2_old))
+        b2_new = (self.b + E2 +
+                  y1 * k12 * (a1_new - a1_old) +
+                  y2 * k22 * (a2_new - a2_old))
 
         if (a1_new > 0) and (a1_new < self.C):
             self.b = b1_new
@@ -240,10 +240,10 @@ class SVC(object):
         E2 = self.E[i2]
         r2 = E2 * y2
 
-        if (r2 < -self.tol and a2 < self.C) or \
-           (r2 > self.tol and a2 > 0):
-            n0nC_list = np.where(np.logical_and(self.alphas != 0,
-                                                self.alphas != self.C))[0]
+        if ((r2 < -self.tol and a2 < self.C) or
+           (r2 > self.tol and a2 > 0)):
+            n0nC_list = np.where((self.alphas != 0) &
+                                 (self.alphas != self.C))[0]
             if len(n0nC_list) > 1:
                 if self.E[i2] > 0:
                     i1 = np.argmin(self.E)
@@ -252,17 +252,17 @@ class SVC(object):
                 if self._take_step(i1, i2):
                     return 1
 
-                np.random.seed(self.random_state)
-                rnd_n0nC_list = np.random.permutation(n0nC_list)
-                for i1 in rnd_n0nC_list:
-                    if self._take_step(i1, i2):
-                        return 1
+            np.random.seed(self.random_state)
+            rnd_n0nC_list = np.random.permutation(n0nC_list)
+            for i1 in rnd_n0nC_list:
+                if self._take_step(i1, i2):
+                    return 1
 
-                np.random.seed(self.random_state)
-                rnd_all_list = np.random.permutation(self.N)
-                for i1 in rnd_all_list:
-                    if self._take_step(i1, i2):
-                        return 1
+            np.random.seed(self.random_state)
+            rnd_all_list = np.random.permutation(self.N)
+            for i1 in rnd_all_list:
+                if self._take_step(i1, i2):
+                    return 1
         return 0
 
     def fit(self, X_train, y_train):
@@ -281,16 +281,14 @@ class SVC(object):
                 for i2 in range(self.N):
                     num_changed += self._examine_example(i2)
             else:
-                i2_list = np.where(np.logical_and(self.alphas != 0,
-                                                  self.alphas != self.C))[0]
+                i2_list = np.where((self.alphas != 0) &
+                                   (self.alphas != self.C))[0]
                 for i2 in i2_list:
                     num_changed += self._examine_example(i2)
 
-            if examine_all:
+            if examine_all == 1:
                 examine_all = 0
-            elif not num_changed:
+            elif num_changed == 0:
                 examine_all = 1
-
-        print(self.b)
 
         return
