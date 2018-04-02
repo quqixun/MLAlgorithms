1056457f41ada5a78e5f41dc2d9912584e2cefc2
Qixun Qu <quqixun@gmail.com>
2018-04-02 18:20:08 +0200

    Fix the mistake

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -23,12 +23,10 @@ import numpy as np
 class SVC(object):
 
     def __init__(self, C=1.0,
-                 kernel="rbf", degree=3,
+                 kernel="rbf", degree=2,
                  sigma="auto", coef0=1.0,
-                 tol=1e-2, epsilon=1e-2,
-                 random_state=None,
-                 verbose=True,
-                 vb_num=10):
+                 tol=1e-3, epsilon=1e-3,
+                 random_state=None):
         '''__INIT__
 
             Instance initialization.
@@ -49,7 +47,6 @@ class SVC(object):
         self.N = None
         self.alphas = None
         self.E = None
-        self.Obj = None
 
         self.X = None
         self.y = None
@@ -66,7 +63,7 @@ class SVC(object):
         self.X = X_train
         self.y = y_train
 
-        self.b = 1.0
+        self.b = 0.0
         self.N, self.F = X_train.shape
 
         if self.sigma == "auto":
@@ -74,7 +71,6 @@ class SVC(object):
 
         self.alphas = np.zeros(self.N)
         self.E = self._E()
-        self.Obj = self._O()
 
         return
 
@@ -90,7 +86,7 @@ class SVC(object):
         ao = alphas ** 2
         yo = self.y ** 2
         ko = self._K(self.X, self.X)
-        obj = 0.5 * np.sum(ao * ko * yo) - np.sum(alphas)
+        obj = np.sum(alphas) - 0.5 * np.sum(ao * ko * yo)
         return obj
 
     def _E(self, index=None):
@@ -116,16 +112,13 @@ class SVC(object):
         '''
 
         if index is not None:
-            if index is None:
-                X = self.X
-            else:
-                X = self.X[index]
+            X = self.X[index]
         else:
             if X is None:
                 X = self.X
 
         pred = np.dot(self.alphas * self.y,
-                      self._K(self.X, X)) + self.b
+                      self._K(self.X, X)) - self.b
 
         return pred
 
@@ -183,10 +176,10 @@ class SVC(object):
         k11 = self._K(x1, x1)
         k12 = self._K(x1, x2)
         k22 = self._K(x2, x2)
-        eta = k11 + k22 - 2 * k12
+        eta = 2 * k12 - k11 - k22
 
-        if eta > 0:
-            a2_new = y2 * (E1 - E2) / eta + a2_old
+        if eta < 0:
+            a2_new = a2_old - y2 * (E1 - E2) / eta
             if a2_new > H:
                 a2_new = H
             elif a2_new < L:
@@ -198,12 +191,17 @@ class SVC(object):
             alphas_temp[i2] = H
             H_obj = self._O(alphas=alphas_temp)
 
-            if L_obj < (H_obj - self.epsilon):
+            if L_obj > (H_obj + self.epsilon):
                 a2_new = L
-            elif L_obj > (H_obj + self.epsilon):
+            elif L_obj < (H_obj - self.epsilon):
                 a2_new = H
             else:
                 a2_new = a2_old
+
+        if a2_new < 1e-8 and a2_new > 0:
+            a2_new = 0.0
+        elif a2_new > (self.C - 1e-8) and a2_new < self.C:
+            a2_new = self.C
 
         if (np.abs(a2_new - a2_old) <
            self.epsilon * (a2_new + a2_old + self.epsilon)):
@@ -218,18 +216,35 @@ class SVC(object):
                   y1 * k12 * (a1_new - a1_old) +
                   y2 * k22 * (a2_new - a2_old))
 
-        if (a1_new > 0) and (a1_new < self.C):
-            self.b = b1_new
-        elif (a2_new > 0) and (a2_new < self.C):
-            self.b = b2_new
+        if a1_new > 0 and a1_new < self.C:
+            b_new = b1_new
+        elif a2_new > 0 and a2_new < self.C:
+            b_new = b2_new
         else:
-            self.b = (b1_new + b2_new) / 2.0
+            b_new = (b1_new + b2_new) / 2.0
 
         self.alphas[i1] = a1_new
         self.alphas[i2] = a2_new
 
-        self.E[i1] = self._E(i1)
-        self.E[i2] = self._E(i2)
+        self.E = self._E()
+
+        if 0.0 < a1_new < self.C:
+            self.E[i1] = 0.0
+
+        if 0.0 < a2_new < self.C:
+            self.E[i2] = 0.0
+
+        # self.E[i1] = self._E(i1)
+        # self.E[i2] = self._E(i2)
+        # self.E = self._E()
+
+        # non_opt = [i for i in range(self.N) if i != i1 and i != i2]
+        # self.E[non_opt] = self.E[non_opt] + \
+        #     y1 * (a1_new - a1_old) * self._K(x1, self.X[non_opt]) + \
+        #     y2 * (a2_new - a2_old) * self._K(x2, self.X[non_opt]) + \
+        #     self.b - b_new
+
+        self.b = b_new
 
         return 1
 
@@ -252,13 +267,11 @@ class SVC(object):
                 if self._take_step(i1, i2):
                     return 1
 
-            np.random.seed(self.random_state)
             rnd_n0nC_list = np.random.permutation(n0nC_list)
             for i1 in rnd_n0nC_list:
                 if self._take_step(i1, i2):
                     return 1
 
-            np.random.seed(self.random_state)
             rnd_all_list = np.random.permutation(self.N)
             for i1 in rnd_all_list:
                 if self._take_step(i1, i2):
@@ -275,6 +288,7 @@ class SVC(object):
         num_changed = 0
         examine_all = 1
 
+        np.random.seed(self.random_state)
         while num_changed > 0 or examine_all:
             num_changed = 0
             if examine_all:
@@ -292,3 +306,8 @@ class SVC(object):
                 examine_all = 1
 
         return
+
+    def predict(self, X_test):
+        pred = self._G(X=X_test)
+        pred = (pred >= 0) * 2 - 1
+        return pred
