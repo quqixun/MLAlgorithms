7e91086168b971900b21bb6da7354d9d1af28415
Qixun Qu <quqixun@gmail.com>
2018-04-10 14:42:22 +0200

    Apply helper functions

--- AdaBoost/src/test.py
+++ AdaBoost/src/test.py
@@ -2,7 +2,7 @@
 # The Script to Test Class "AdaBoostTree".
 # Author: Qixun Qu
 # Create on: 2018/03/13
-# Modify on: 2018/04/09
+# Modify on: 2018/04/10
 
 #     ,,,         ,,,
 #   ;"   ';     ;'   ",
@@ -17,54 +17,57 @@
 #       '&$$$$$&'
 
 
-import numpy as np
+from __future__ import print_function
+
+
+from utils import *
 from AdaBoostTree import *
 from sklearn.datasets import make_hastie_10_2
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
 
 
-seed = 9527
+# Basic settings
+random_state = 9527
 n_samples = 10000
 test_size = 0.2
 
+
 # Step 1
 # Generate Dataset for training and testing.
-
+# Obtain all samples
 X, y = make_hastie_10_2(n_samples=n_samples,
-                        random_state=seed)
-data = np.concatenate([X, np.reshape(y, (-1, 1))], axis=1)
-train, test = train_test_split(data, test_size=test_size,
-                               random_state=seed)
-
-X_train, y_train = train[:, :-1], train[:, -1]
-X_test, y_test = test[:, :-1], test[:, -1]
+                        random_state=random_state)
+# Split dataset
+X_train, y_train, X_test, y_test = split_dataset(X, y, test_size,
+                                                 random_state)
+# Normalize dataset
+X_train_scaled, X_test_scaled = scale_dataset(X_train, X_test)
 
 
 # Step 2
 # Generate the basic classifier.
-
 clf = DecisionTreeClassifier(criterion="entropy",
                              max_depth=3,
-                             random_state=325)
+                             random_state=random_state)
 
 
 # Step 3
 # Test class "AdaBoostTree"
-
 # Set the number of iterations
 M = 200
 
 # Initialize the object
-abt = AdaBoostTree(M, clf,
-                   verbose=True, vb_num=10)
+abt = AdaBoostTree(M, clf, verbose=True, vb_num=10)
 
 # Fit training data
-abt.fit(X_train, y_train, X_test, y_test)
-# abt.fit(X_train, y_train)
+abt.fit(X_train_scaled, y_train,
+        X_test_scaled, y_test)
+# abt.fit(X_train_scaled, y_train)
 
 # Predict test data
-abt.predict(X_test)
+y_pred = abt.predict(X_test_scaled)
+print("Accuracy of test set:", accuracy(y_pred, y_test))
+# Accuracy could reach 0.9370.
 
 # Plot learning curves
 abt.plot_curve()
