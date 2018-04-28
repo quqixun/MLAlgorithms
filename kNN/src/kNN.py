7f9b77ab65e90bf10a2adfaf737dc9870cec902b
Qixun Qu <quqixun@gmail.com>
2018-04-28 22:42:15 +0800

    Finish building kdtree

--- kNN/src/kNN.py
+++ kNN/src/kNN.py
@@ -2,7 +2,7 @@
 # Class of "kNN".
 # Author: Qixun Qu
 # Create on: 2018/04/26
-# Modify on: 2018/04/26
+# Modify on: 2018/04/28
 
 #     ,,,         ,,,
 #   ;"   ';     ;'   ",
@@ -24,6 +24,22 @@ from __future__ import print_function
 import numpy as np
 
 
+class Node(object):
+
+    def __init__(self, data,
+                 left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+        return
+
+    def get_left(self):
+        return self.left
+
+    def get_right(self):
+        return self.right
+
+
 class kNN(object):
 
     def __init__(self, k=10):
@@ -33,15 +49,45 @@ class kNN(object):
         self.k = k
         self.kd_tree = None
 
+        self.X = None
+        self.y = None
+
+        self.N = None
+        self.F = None
+
         return
 
-    def _build_kd_tree(self):
+    def _initialize(self, X, y):
+        '''_INITIALIZE
+        '''
+
+        self.X = X
+        self.y = y
+
+        self.N, self.F = X.shape
+
+        return
+
+    def _build_kd_tree(self, X=None, depth=0):
         '''_BUILD_KD_TREE
         '''
 
-        return
+        if X is None:
+            X = self.X
 
-    def _search_kd_tree(self):
+        if len(X) == 0:
+            return None
+        elif len(X) == 1:
+            return Node(data=X[0])
+        else:
+            fth = depth % self.F
+            X = X[np.argsort(X[:, fth]), :]
+            m = len(X) // 2
+            return Node(data=X[m],
+                        left=self._build_kd_tree(X[:m], depth + 1),
+                        right=self._build_kd_tree(X[m + 1:], depth + 1))
+
+    def _search_kd_tree(self, x):
         '''_SEARCH_KD_TREE
         '''
 
@@ -51,6 +97,9 @@ class kNN(object):
         '''FIT
         '''
 
+        self._initialize(X, y)
+        self.kdtree = self._build_kd_tree()
+
         return
 
     def predict(self, X):
@@ -58,3 +107,17 @@ class kNN(object):
         '''
 
         return
+
+
+def main():
+    """Example usage"""
+    X = np.array([(2, 3), (5, 4), (9, 6), (4, 7), (8, 1), (7, 2)])
+    knn = kNN()
+    knn.fit(X, None)
+    print(knn.kdtree.data)
+    # tree = kdtree(point_list)
+    # print(tree)
+
+
+if __name__ == '__main__':
+    main()
