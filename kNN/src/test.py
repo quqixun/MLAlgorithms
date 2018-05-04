bf668559d294613ce84cdf8d325b2292a9db9566
Qixun Qu <quqixun@gmail.com>
2018-05-04 23:31:29 +0800

    Test kNN on simulation dataset

--- kNN/src/test.py
+++ kNN/src/test.py
@@ -2,7 +2,7 @@
 # The Script to Test Class "kNN".
 # Author: Qixun Qu
 # Create on: 2018/04/30
-# Modify on: 2018/04/30
+# Modify on: 2018/05/04
 
 #     ,,,         ,,,
 #   ;"   ';     ;'   ",
@@ -23,21 +23,23 @@ from __future__ import print_function
 from utils import *
 from kNN import kNN
 from sklearn.datasets import make_blobs
-from sklearn.datasets import make_hastie_10_2
 
 
 # Basic settings
-random_state = 9527
-n_samples = 10000
-test_size = 0.2
+random_state = 9527  # Seed
+n_samples = 10000    # Number of samples
+centers = 5          # Number of clusters
+n_features = 5       # Number of features
+test_size = 0.2      # Proportion of test set
 
 
 # Generate Dataset for training and testing.
 # Obtain all samples
-# X, y = make_hastie_10_2(n_samples=n_samples,
-#                         random_state=random_state)
-X, y = make_blobs(n_samples=n_samples, centers=2,
-                  n_features=2, random_state=random_state)
+X, y = make_blobs(n_samples=n_samples,
+                  centers=centers,
+                  n_features=n_features,
+                  cluster_std=3,
+                  random_state=random_state)
 # Split dataset
 X_train, y_train, X_test, y_test = split_dataset(X, y, test_size,
                                                  random_state)
@@ -45,9 +47,25 @@ X_train, y_train, X_test, y_test = split_dataset(X, y, test_size,
 X_train_scaled, X_test_scaled = scale_dataset(X_train, X_test)
 
 
-knn = kNN(k=100)
+# Build kd-tree
+knn = kNN()
 knn.fit(X_train_scaled, y_train)
 
-# Predict test data
-y_pred = knn.predict(X_test_scaled)
-print("Accuracy of test set:", accuracy(y_pred, y_test))
+# Do prediction using different
+# number of nearest neighbors
+n, step = 100, 10
+K = list(range(0, n + 1, step))
+K[0] = 1
+
+test_accs = []
+for k in K:
+    # Predict test samples
+    knn.set_k(k)
+    test_pred = knn.predict(X_test_scaled)
+    test_acc = accuracy(test_pred, y_test)
+    print("Accuracy of test dataset is: ", test_acc)
+    # The result can reach 0.9725
+    test_accs.append(test_acc)
+
+# Plot accuracies over different k
+plot_curve(K, test_accs)
